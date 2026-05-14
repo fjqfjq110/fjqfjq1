4554dc8dc8bc2aa8d643064c721f127f1dd77ffc
wuzhenggang <13506582782@163.com>
2026-05-14 17:08:49 +0800

    refactor：重构app.vue页面，通过路由访问

--- front/vite-project/src/main.js
+++ front/vite-project/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import router from './router'
+import App from './App.vue'
 
-createApp(App).use(ElementPlus).mount('#app')
+createApp(App).use(router).use(ElementPlus).mount('#app')
